8e4f891e18650ab83a28cc7dfb29974fccbd472c
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2024-07-28 19:28:29 +0900

    Update run.sh

--- keras-benchmarks/shell/run.sh
+++ keras-benchmarks/shell/run.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# Define the path to virtual environments and the output file
 venv_path=~/.venv
-venvs=("tensorflow" "keras-tensorflow" "keras-jax" "keras-torch")
+venvs=(
+    "keras-tensorflow"
+)
 output_file=results.txt
 
-# Remove the output file if it already exists
 if [ -e "$output_file" ]; then
     rm -f "$output_file"
 fi
 
-# Set environment variables
 export LD_LIBRARY_PATH=
 export NVIDIA_TF32_OVERRIDE=0
 
-# Define the models to benchmark
-models=("gpt2")
+models=(
+    #"bert"
+    #"sam"
+    #"stable_diffusion"
+    #"gemma"
+    #"mistral"
+    "gpt2"
+)
 
-# Initialize the output file with the header
 printf "# Benchmarking $venv_name\n\n" | tee -a $output_file
 
-# Set the KERAS_HOME environment variable
 export KERAS_HOME=configs/keras-tensorflow
 
-# Loop through each model and benchmark
 for model_name in "${models[@]}"; do
-    printf "$model_name:\n" | tee -a $output_file
-    printf "fit:\n" | tee -a $output_file
-    python3.11 benchmark/$model_name/fit.py $output_file
-    printf "\n\n" | tee -a $output_file
+printf "$model_name:\n" | tee -a $output_file
+printf "fit:\n" | tee -a $output_file
+python3.11 /workspace/keras-benchmarks/benchmark/$model_name/fit_ar_nccl.py $output_file
+printf "\n\n" | tee -a $output_file
 done
