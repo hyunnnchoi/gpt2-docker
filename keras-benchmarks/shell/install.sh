bd90795798faaf9c699b3672adf8a35b894d1fd7
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2024-07-28 16:41:58 +0900

    Update install.sh

--- keras-benchmarks/shell/install.sh
+++ keras-benchmarks/shell/install.sh
@@ -1,4 +1,6 @@
 pip install --upgrade pip
-pip install -r requirements/keras-common.txt
-pip install keras==3.2.0 #3.0.5
+pip install tensorflow[and-cuda]==2.16.2
+pip install keras==3.4.1
+pip install "keras-nlp>=0.8.2"
+pip install "keras-cv>=0.8.2"
 pip install -e .
