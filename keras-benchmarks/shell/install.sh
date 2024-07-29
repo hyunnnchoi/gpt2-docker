980f7bc1ce44a38928a63e70f9318e881a3c16fa
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2024-07-29 10:37:56 +0900

    Update install.sh

--- keras-benchmarks/shell/install.sh
+++ keras-benchmarks/shell/install.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 pip install --upgrade pip
-pip install -r /workspace/keras-benchmarks/requirements/keras-common.txt
-pip install keras==3.2.0
+pip install -r /workspace/keras-benchmarks/requirements/hmchoi.txt
+pip install keras==3.2.0 #3.0.5
 pip install keras-nlp
 pip install -e /workspace/keras-benchmarks
+
+echo "Installed libraries from hhlee.txt"
